af95063c9cd2258ade8d0df21451134190e58def
AgungWicaksonoPutro <agungwicaksonoputro.dev@gmail.com>
2020-09-12 16:52:57 +0700

    Add Category Page

--- src/App.vue
+++ src/App.vue
@@ -9,12 +9,27 @@
 </template>
 
 <style>
+@font-face {
+font-family: 'Metropolis-Bold';
+src: url('../src/assets/font/Metropolis-Bold.otf');
+}
+@font-face {
+font-family: 'Metropolis-Light';
+src: url('../src/assets/font/Metropolis-Light.otf');
+}
+@font-face {
+font-family: 'Metropolis-Medium';
+src: url('../src/assets/font/Metropolis-Medium.otf');
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Metropolis-Light', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  margin: 0;
+  padding: 0;
 }
 
 #nav {
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Category from '../views/Category/Category.vue'
 
 Vue.use(VueRouter)
 
@@ -9,6 +10,11 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/category',
+    name: 'Category',
+    component: Category
   },
   {
     path: '/about',
