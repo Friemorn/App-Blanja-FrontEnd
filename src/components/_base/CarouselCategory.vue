0be91bd6501e9ff74a471fda010ceb4c38be8517
AgungWicaksonoPutro <agungwicaksonoputro.dev@gmail.com>
2020-09-13 20:56:50 +0700

    Add Caraousel eror button category

---
+++ src/components/_base/CarouselCategory.vue
@@ -0,0 +1,105 @@
+<template>
+<carousel class="carousel"
+  :navigationEnabled="true"
+  :navigation-next-label="navigationNextCategory()"
+  :navigation-prev-label="navigationPrevCategory()"
+  :perPageCustom="[[768, 4], [1024, 4]]"
+  >
+  <slide class="slide">
+    <img src="../../assets/hiclipart 15.png" alt="">
+    <label for="slide">Black Edition</label>
+  </slide>
+  <slide class="slide">
+    <img src="../../assets/hiclipart 21.png" alt="">
+    <label for="slide">Black Edition</label>
+  </slide>
+  <slide class="slide">
+    <img src="../../assets/hiclipart 22.png" alt="">
+    <label for="slide">Black Edition</label>
+  </slide>
+  <slide class="slide">
+    <img src="../../assets/hiclipart 26.png" alt="">
+    <label for="slide">Black Edition</label>
+  </slide>
+  <slide class="slide">
+    <img src="../../assets/hiclipart 26.png" alt="">
+    <label for="slide">Black Edition</label>
+  </slide>
+</carousel>
+</template>
+
+<script>
+import { Carousel, Slide } from 'vue-carousel'
+export default {
+  name: 'CarouselCategory',
+  components: {
+    Carousel,
+    Slide
+  },
+  methods: {
+    navigationNextCategory: function () { return '<button class="btn-right category"><i class="fas fa-chevron-right"></i></button>' },
+    navigationPrevCategory: function () { return '<button class="btn-left category"><i class="fas fa-chevron-left"></i></button>' }
+  }
+}
+</script>
+
+<style scoped>
+.VueCarousel-slide {
+  min-width: 70px;
+  position: relative;
+  background: #42b983;
+  color: #fff;
+  font-family: Arial;
+  font-size: 24px;
+  text-align: center;
+  height: 70px;
+  border-radius: 10px;
+  margin: 10px;
+}
+.VueCarousel-slide img{
+  border-radius: 10px;
+  width: 100%;
+  height: 100%;
+  object-fit: contain;
+}
+.VueCarousel-pagination[data-v-438fd353]{
+  text-align: left;
+}
+label {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  font-family: 'Metropolis-Medium';
+  font-weight: bold;
+  font-size: 18px;
+  line-height: 18px;
+  letter-spacing: 2px;
+}
+.VueCarousel-navigation-prev[data-v-453ad8cd]{
+    color: lawngreen;
+}
+.btn-left.category{
+  position: absolute;
+  width: 35px;
+  height: 35px;
+  left: 60px;
+  top: -50px;
+  border-radius: 50px;
+  border: none;
+  background: #FFFFFF;
+  box-shadow: 0px 4px 10px rgba(181, 181, 181, 0.25);
+}
+
+.btn-right.category{
+  position: absolute;
+  width: 35px;
+  height: 35px;
+  right: 60px;
+  top: -50px;
+  border-radius: 50px;
+  border: none;
+  background: #FFFFFF;
+  box-shadow: 0px 4px 10px rgba(181, 181, 181, 0.25);
+}
+</style>
--- src/views/Home/Homepage.vue
+++ src/views/Home/Homepage.vue
@@ -1,14 +1,24 @@
 <template>
-<div class="container-fluid">
+<div>
   <div class="row no-gutters">
     <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
       <CarouselTrend/>
     </div>
   </div>
-  <div class="row no-gutters">
-    Category
+  <div class="row no-gutters category">
+    <div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col-3 my-3">
+      <div class="row no-gutters">
+        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+          <h4 class="text-left">Category</h4>
+          <p class="text-left">What are you currently looking for</p>
+        </div>
+      </div>
+    </div>
+    <div class="col-xl-9 col-lg-9 col-md-9 col-sm-9 col-9 my-3">
+      <CarouselCategory/>
+    </div>
   </div>
-  <div class="row no-gutters new mx-5 my-4">
+  <div class="row no-gutters new my-3 body">
     <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 text-left">
       <div class="row no-gutters header">
         <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 text-left">
@@ -16,7 +26,7 @@
         <p>You’ve never seen it before!</p>
         </div>
       </div>
-      <div class="row no-gutters body">
+      <div class="row no-gutters">
         <div class="col-xl-12 col-lg-12 col-md-6 col-sm-6 col-12 text-left d-flex flex-wrap">
         <CardProduct/>
         <CardProduct/>
@@ -28,7 +38,7 @@
       </div>
     </div>
   </div>
-  <div class="row no-gutters popular mx-5 my-4">
+  <div class="row no-gutters my-3  popular body">
     <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 text-left">
       <div class="row no-gutters header my-3">
         <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 text-left">
@@ -36,7 +46,7 @@
           <p>Find clothes that are trending recently</p>
         </div>
       </div>
-      <div class="row no-gutters body">
+      <div class="row no-gutters">
         <div class="col-xl-12 col-lg-12 col-md-2 col-sm-6 col-12 text-left d-flex flex-wrap">
           <CardProduct/>
           <CardProduct/>
@@ -53,11 +63,13 @@
 <script>
 import CardProduct from '../../components/_base/CardProduct'
 import CarouselTrend from '../../components/_base/CarouselTrend'
+import CarouselCategory from '../../components/_base/CarouselCategory'
 export default {
   name: 'Homepage',
   components: {
     CardProduct,
-    CarouselTrend
+    CarouselTrend,
+    CarouselCategory
   }
 }
 </script>
@@ -71,5 +83,14 @@ h4{
 p{
   letter-spacing: 1px;
   margin: 0;
+}
+.category{
+  padding-left: 100px;
+  padding-right: 100px;
+  background-color: #F0F1F9;
+}
+.body {
+  padding-left: 100px;
+  padding-right: 100px;
 }
 </style>
